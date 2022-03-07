e0f500f0d7cdfb08ccc455e68e76a2b3f8d45936
shane <shane@groundedpackaging.co>
2022-03-07 21:32:37 +0700

    cypress and react testing library

---
+++ packages/client/cypress/integration/payment.test.js
@@ -0,0 +1,10 @@
+describe("payment", () => {
+  it("user can make payment via QR code", () => {
+    cy.visit("/");
+    // cy.visit(`${Cypress.env("clientUrl")}`);
+    //visit the site
+    //login
+    //create an order
+    //make a payment
+  });
+});
---
+++ packages/client/cypress/support/commands.js
@@ -0,0 +1,1 @@
+import "@testing-library/cypress/add-commands"; //so we can use same command as in react-testing-library
