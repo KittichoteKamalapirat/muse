a568798f20205d3039ce1bbdaad53601f9a81db0
shane <shane@groundedpackaging.co>
2022-04-11 00:01:04 +0700

    need to checkout

--- packages/server/src/migrations/1649608243051-createNewPost.ts
+++ packages/server/src/migrations/1649608243051-createNewPost.ts
@@ -2,12 +2,10 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createNewPost1649608243051 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(``);
     await queryRunner.query(`START TRANSACTION`);
     await queryRunner.query(
       `INSERT INTO "post"("title", "text", "instruction", "advice", "cooktime", "portion", "points", "thumbnailUrl", "videoUrl", "creatorId", "ingredients", "createdAt", "updatedAt") VALUES ($1, $2, $3, $4, $5, $6, DEFAULT, $7, $8, $9, $10, DEFAULT, DEFAULT) RETURNING "id", "points", "createdAt", "updatedAt" -- PARAMETERS: ["Marinara migration","Marinara migration Details",["boil pasta","make sauce","mix"],["tips"],"5",2,"https://cookknow.s3.amazonaws.com/cherry tomato.png","https://cookknow.s3.amazonaws.com/cherry tomato.mp4","e0930888-50c8-4172-9d80-9ba13cbe4d01","[{\"ingredient\":\"garlic\",\"amount\":\"10\",\"unit\":\"gram\"},{\"ingredient\":\"pasta\",\"amount\":\"20\",\"unit\":\"gram\"}]"]`
     );
-
     await queryRunner.query(`COMMIT`);
 
     await queryRunner.query(`START TRANSACTION`);
@@ -17,5 +15,7 @@ export class createNewPost1649608243051 implements MigrationInterface {
     await queryRunner.query(`COMMIT`);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // comment
+  }
 }
