9aac3f6cbb741aaf8fb814f205cbb17b5f65c330
shane <shane@groundedpackaging.co>
2022-04-13 23:41:52 +0700

    setup graphql hooks tests

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -13,6 +13,12 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- packages/server/src/__test__/redis.test.ts
+++ packages/server/src/__test__/redis.test.ts
@@ -1,9 +1,43 @@
-import Redis from "ioredis";
+/* eslint-disable no-undef */
+import { Server } from "http";
+import { Connection } from "typeorm";
+import { v4 } from "uuid";
+import User from "../entities/User";
+import { startServer } from "../startSever";
 
-describe("redis tests", () => {
-  it("ping", async () => {
-    const redis = new Redis();
+let expressServer: Server;
+let postgresDb: Connection;
 
-    await redis.ping();
+beforeAll(async () => {
+  // const conn = await createTypeORMConn(); //drop a schema, config in ormconfig for test
+  const serverAndDb = await startServer();
+  expressServer = serverAndDb.server;
+  postgresDb = serverAndDb.connection;
+
+  console.log({ postgresDb });
+
+  postgresDb.options.entities?.forEach((item) => {
+    console.log({ item });
+  });
+});
+
+afterAll(async () => {
+  await postgresDb.close; // error -> Jest did not exit one second after the test run has completed
+  await expressServer.close(); // close express ? close postgres
+});
+
+describe("db tests", () => {
+  it("create user", async () => {
+    const uuid = v4();
+    const data = {
+      username: "User1",
+      email: "user1@gmail.com",
+      phonenumber: "0900000000",
+      password: "123",
+      isCreator: false,
+      avatar: `https://avatars.dicebear.com/api/open-peeps/${uuid}.svg`,
+    };
+    const user = User.create(data);
+    return user.save();
   });
 });
