6fbcf3b998421bef2faa0ab5496a7817ad2e3d4c
shane <shane@groundedpackaging.co>
2022-05-17 00:04:42 +0700

    fix frontend jest tests

--- packages/client/src/components/form/FormActionButtons/FormActionButtons.test.tsx
+++ packages/client/src/components/form/FormActionButtons/FormActionButtons.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import FormActionButtons from "./FormActionButtons";
+import "@testing-library/jest-dom";
 
 describe("FormActionButtons", () => {
   test("renders default buttons", () => {
