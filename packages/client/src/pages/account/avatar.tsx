b29f16a95fcef9420550747b3bb29a81ee12b64e
shane <shane@groundedpackaging.co>
2022-06-14 00:37:22 +0700

    add if error

--- packages/client/src/pages/account/avatar.tsx
+++ packages/client/src/pages/account/avatar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HeadingLayout } from "../../components/Layout/HeadingLayout";
 import { Layout } from "../../components/Layout/Layout";
 import { SingleFileUpload } from "../../components/SingleFileUpload";
+import { Error } from "../../components/skeletons/Error";
 import { Loading } from "../../components/skeletons/Loading";
 import { XWrapper } from "../../components/Wrapper/XWrapper";
 import { useMeQuery, useUpdateAvatarMutation } from "../../generated/graphql";
@@ -9,7 +10,7 @@ import { withApollo } from "../../util/withApollo";
 
 const Avatar = () => {
   const [updateAvatar] = useUpdateAvatarMutation();
-  const { data: meData, loading } = useMeQuery();
+  const { data: meData, loading, error } = useMeQuery();
 
   if (loading) {
     return (
@@ -18,6 +19,15 @@ const Avatar = () => {
       </Layout>
     );
   }
+
+  if (error) {
+    return (
+      <Layout heading="error">
+        <Error text={error.message} />
+      </Layout>
+    );
+  }
+
   return (
     <HeadingLayout heading="Update my avatar">
       <XWrapper>
