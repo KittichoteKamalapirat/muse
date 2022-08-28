2385fe0dc0cf1629815f7d6863f0cc888dd07318
shane <shane@groundedpackaging.co>
2022-08-28 23:28:51 +0700

    add websocket and it's working

--- packages/websocket/dist/index.js
+++ packages/websocket/dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const socket_io_1 = require("socket.io");
-const io = new socket_io_1.Server(3003, {});
+const io = new socket_io_1.Server(3003, {
+    cors: {
+        origin: ["http://localhost:19000", "http://localhost:19001"],
+    },
+});
 io.on("connection", (socket) => {
     console.log("id", socket.id);
     console.log("socket started");
+    socket.on("incrementUpvote", (placeId, songId, upvotesNum) => {
+        console.log("placeId", placeId);
+        console.log("songId", songId);
+        console.log("upvotesNum", upvotesNum);
+        io.emit("broadcastIncrementUpvote", placeId, songId, upvotesNum + 1);
+    });
 });
 //# sourceMappingURL=index.js.map
--- packages/websocket/src/index.ts
+++ packages/websocket/src/index.ts
@@ -1,10 +1,18 @@
 import { Server } from "socket.io";
 
 const io = new Server(3003, {
-  /* options */
+  cors: {
+    origin: ["http://localhost:19000", "http://localhost:19001"],
+  },
 });
 
 io.on("connection", (socket) => {
   console.log("id", socket.id);
   console.log("socket started");
+  socket.on("incrementUpvote", (placeId, songId, upvotesNum) => {
+    console.log("placeId", placeId);
+    console.log("songId", songId);
+    console.log("upvotesNum", upvotesNum);
+    io.emit("broadcastIncrementUpvote", placeId, songId, upvotesNum + 1);
+  });
 });
